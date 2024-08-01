3fc312c6442881b258626c056e5e6757daadaf9b
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-01 21:56:49 +0300

    Created ExcavatorProducer and implements method get.

---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,29 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+    private List<Excavator> excavators = new ArrayList<>();
+
+    @Override
+    public List<Excavator> get() {
+        Excavator excavator1 = new Excavator();
+        Excavator excavator2 = new Excavator();
+        Excavator excavator3 = new Excavator();
+        excavators.add(excavator1);
+        excavators.add(excavator2);
+        excavators.add(excavator3);
+        excavator1.setColor("WHITE");
+        excavator1.setName("John Deere");
+        excavator1.setDippingDepth(5);
+        excavator2.setColor("PURPLE");
+        excavator2.setName("Caterpillar");
+        excavator2.setDippingDepth(10);
+        excavator3.setColor("GREY");
+        excavator3.setName("Volvo");
+        excavator3.setDippingDepth(8);
+        return excavators;
+    }
+}
