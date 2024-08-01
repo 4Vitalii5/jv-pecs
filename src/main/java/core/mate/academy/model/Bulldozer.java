ee91983a64b0134cf325700d24561185264ef3dd
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-01 21:55:16 +0300

    Added field frontBlade,

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -1,9 +1,5 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Bulldozer
- * Do not remove no-args constructor
- */
 public class Bulldozer extends Machine {
     private boolean frontBlade;
 
@@ -12,10 +8,6 @@ public class Bulldozer extends Machine {
     }
 
     public Bulldozer() {
-    }
-
-    public boolean isFrontBlade() {
-        return frontBlade;
     }
 
     public void setFrontBlade(boolean frontBlade) {
